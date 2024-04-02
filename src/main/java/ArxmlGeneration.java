dccdd5c324414df635969202dff2fd144bb2768e
Nada Ayman <nadaaymanatia@aucegypt.edu>
2024-04-03 00:21:20 +0200

    generating arxml and multiplicity

---
+++ src/main/java/ArxmlGeneration.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Johnny
+ */
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+public class ArxmlGeneration {
+    
+}
